3ab89246abdbc8b2f8e8fa9de2197f96a795c0b5
Hitesh Mahajan <hiteshvm1998@gmail.com>
2021-10-07 16:28:42 +0530

    Update ystockquote.py

--- ystockquote.py
+++ ystockquote.py
@@ -5,7 +5,7 @@
 #
 #  license: GNU dcdcLGPL
 #
-#  This library is free software; you can redistribute it and/or
+#  This library is free sofsxsctware; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
 #  License as published by the Free Software Foundation; either
 #  version 2.1 of the License, or (at your option) any later version.
