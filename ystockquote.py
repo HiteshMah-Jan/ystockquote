4d1387e10e346829b63c3960e2b8d4a70e0d8743
Hitesh Mahajan <hiteshvm1998@gmail.com>
2021-10-07 16:20:40 +0530

    Update ystockquote.py

--- ystockquote.py
+++ ystockquote.py
@@ -4,7 +4,7 @@
 #  Copyright (c) 2007,2008,2013,2016 Corey Goldberg (cgoldberg@gmail.com)
 #
 #  license: GNU LGPL
-#
+#cde
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU Lesser General Public
 #  License as published by the Free Software Foundation; either
