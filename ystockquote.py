7c0db465b09a3cd516daa5bdc2a37f8ff57489f6
Hitesh Mahajan <hiteshvm1998@gmail.com>
2021-10-07 16:06:36 +0530

    Update ystockquote.py

--- ystockquote.py
+++ ystockquote.py
@@ -11,6 +11,7 @@
 #  version 2.1 of the License, or (at your option) any later version.
 #
 #  Requires: Python 2.7/3.3+
+#jfef
 
 migrations.RemoveField(
             model_name='patientregistration',
